155b6622df9ca9375636ae617999e6b510a12fd4
CTBlankenship <chthbl@protonmail.com>
2019-03-20 19:10:11 -0400

    18. Create model and add to Database

---
+++ BookList_Razor/Model/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace BookList_Razor.Model
+{
+    public class ApplicationDbContext : DbContext        
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
